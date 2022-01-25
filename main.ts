6154e83bf70e42c578236fdf57c6d4c6717dcb8a
thanaphonounvises <48167851+thanaphonounvises@users.noreply.github.com>
2022-01-25 13:43:52 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+koi.koi_onbtn(function (btnA, btnB) {
+	
+})
+koi.koi_cloud_onfindface(function (name, confidence) {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    koi.koi_screenshot("name.jpg")
+})
+koi.koi_init(SerialPin.P1, SerialPin.P2)
+basic.forever(function () {
+	
+})
